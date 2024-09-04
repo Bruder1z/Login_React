92556321e2dd5e5e71e9335e298464f8064fac91
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2024-09-04 11:47:26 -0300

    Add files via upload

---
+++ login react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './Components/Login/Login';
+
+function App() {
+  
+
+  return (
+
+    <div className="App">
+      <Login />
+    </div>
+
+  );
+  
+}
+
+export default App
